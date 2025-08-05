694c8bb4c07e6b11fb5cf0a4087f731c7afe0039
mahendharjillala <jillalamahendhar@gmail.com>
2025-08-05 12:27:55 +0530

    Initial commit: Frontend deployed with Vite + Jenkins + Tomcat

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S219_CICD">
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
