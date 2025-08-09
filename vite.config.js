4dae23e6c1a3874e4e6cae8da4e09fd9fca21bcb
Jillala Mahendhar <139626780+mahendharjillala@users.noreply.github.com>
2025-08-09 16:00:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/S218JenkinsFrontend/'
-  // base: '/myapp/'
+  // base: '/S218JenkinsFrontend/'
+  base: '/myapp/'
 });
