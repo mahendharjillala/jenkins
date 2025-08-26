cefa5331d1cf510d7f38da0cbfdc6b9d8cf88d11
Jillala Mahendhar <139626780+mahendharjillala@users.noreply.github.com>
2025-08-26 11:52:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename='/myapp/'>
+    <Router basename='/jenkins/'>
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
